25f30c2792272b9b4055ef658ffa8a87c5b0041f
Njaaga-Gakure <njaagagakure@gmail.com>
2023-01-15 16:18:43 +0300

    Style navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import logo from "../react-logo.png"
+import logo from "../images/react-icon-small.png"
 
 export default function Header(){
     return (
-        <header>
             <nav className="nav">
-                <img src={logo} className="nav-logo" alt="react logo"/>
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contacts</li>
-                </ul>
+                <img src={logo} className="nav--logo" alt="react logo" />
+                <h3 className="nav--logo-text">ReactFacts</h3>
+                <h4 className="nav--title">React Course - Project 1</h4>
             </nav>
-        </header>
     )
 }
