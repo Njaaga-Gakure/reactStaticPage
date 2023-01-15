4199c1356944c14b965675067cc9a91e9ba8a051
Njaaga-Gakure <njaagagakure@gmail.com>
2023-01-15 12:40:17 +0300

    Create parent and child components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-function MainContent() {
+import React from "react";
+
+function Header(){
+    return (
+        <header>
+            <img src="./react-logo.png" width="40px" alt="react logo"/>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contacts</li>
+            </ul>
+        </header>
+    )
+
+}
+function MainContent(){
+    return (
+        <>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </>
+    )
+}
+function Footer(){
+    return (
+        <footer>
+            <small>© 2023 Gakure development. All rights reserved.</small>
+        </footer>
+    )
+
+}
+
+export function Page(){
         return (
-            <div>
-                <img src="./react-logo.png" width="40px" alt="react logo"/>
-                <h1>Fun facts about React</h1>
-                <ul>
-                    <li>Was first released in 2013</li>
-                    <li>Was originally created by Jordan Walke</li>
-                    <li>Has well over 100k stars on Github</li>
-                    <li>Is maintained by Facebook</li>
-                    <li>Powers thousands of enterprise apps, including mobile apps</li>
-                </ul>
-            </div>
+            <>
+                <Header />
+                <MainContent />
+                <Footer />
+            </>
         )
 }
-export default MainContent
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MainContent from './App'
+import {Page} from './App'
 
 /**
  * the old React way
  * ReactDOM.render(< MainContent />, document.getElementById("root"))
  **/
-ReactDOM.createRoot(document.getElementById("root")).render(<MainContent />)
+ReactDOM.createRoot(document.getElementById("root")).render(<Page />)
